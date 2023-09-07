af399e5e7b9c2aee8455d79cc91dbfd4c94862c4
cksghks89 <song960705@naver.com>
2023-09-07 09:14:25 +0900

    fix: closet 도메인의 entity에 @ManyToOne fetch 전략을 LAZY로 수정

--- src/main/java/com/dadada/onecloset/domain/closet/entity/Closet.java
+++ src/main/java/com/dadada/onecloset/domain/closet/entity/Closet.java
@@ -25,7 +25,7 @@ public class Closet extends BaseTimeEntity {
 
     private String iconColor;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
