64f526339119ec6b8c4065bbbd4227f2e1bb229b
지민성 <jiminsung@naver.com>
2023-09-14 16:47:41 +0900

    fix: healthCheckController 변경

--- src/main/java/com/dadada/onecloset/global/HealthCheckController.java
+++ src/main/java/com/dadada/onecloset/global/HealthCheckController.java
@@ -3,16 +3,11 @@ package com.dadada.onecloset.global;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Locale;
-
 @RestController
 public class HealthCheckController {
 
     @GetMapping("/")
     public String healthCheck() {
-        String hexString = "0xFF000000";
-        Long longValue = Long.decode(hexString);
-        System.out.println(longValue);
         return "dadada";
     }
 
